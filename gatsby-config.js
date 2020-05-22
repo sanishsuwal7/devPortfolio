2ae41323fbfd94e2ece707967e26b30ff2393c12
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-05-22 13:35:42 -0400

    added route to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
