aaffd34e7692db5a513d38c26767f63fcec776ff
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-06-05 18:08:33 -0400

    styled components added to gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
 
     {
       resolve: `gatsby-source-filesystem`,
