9b548125e4865770be52344399439aa5b3d27603
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-08-12 10:32:35 -0400

    content

--- src/components/SkillIcons.jsx
+++ src/components/SkillIcons.jsx
@@ -10,11 +10,11 @@ export default function SkillIcons() {
         <li>Python</li>
         <li>React</li>
         <li>NodeJS</li>
+        <li>GraphQL</li>
+        <li>Sequelize</li>
         <li>Unity3D</li>
         <li>C#</li>
         <li>C++</li>
-        <li>GraphQL</li>
-        <li>NoSQL</li>
       </AllTags>
     </Wrapper>
   )
