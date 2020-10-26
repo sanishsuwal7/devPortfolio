815e64eb2a65d492a614d2d528e1a103561cde1c
Alejandro Aspinwall <aaspinwall@gmail.com>
2020-10-25 20:06:40 -0400

    twitter icon and ing thumbnail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer style={{ padding: "3rem 1rem 1rem" }}>
       © {new Date().getFullYear()}, Built and designed by Alejandro Aspinwall,
-      follow him on <a href="https://www.gatsbyjs.org">Twitter</a>
+      follow him on <a href="https://aaspinwall.com/">Twitter</a>
       <a href=""></a>
     </footer>
   )
