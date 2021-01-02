466f887ccd374a7ebe588b54af5a90c01b51ff3b
Alejandro Aspinwall <aaspinwall@gmail.com>
2021-01-02 16:55:17 -0500

    muted video

--- content/blog/cloud-resume/Video.jsx
+++ content/blog/cloud-resume/Video.jsx
@@ -6,6 +6,7 @@ const Video = () => {
       style={{ margin: "2rem auto", display: "grid" }}
       autoPlay={true}
       loop={true}
+      muted={true}
       src="https://user-images.githubusercontent.com/17233773/102701827-43971800-4229-11eb-89b7-95b2ec42760b.mov"
     ></video>
   )
